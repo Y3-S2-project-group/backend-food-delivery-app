533a93540ed11f5b45e046099d8d49c90b6c4f1f
Nipun Jayasinghe <nipunprjay@gmail.com>
2025-04-27 16:48:40 +0530

    bug: server.js updated!

--- payment-service/server.js
+++ payment-service/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-
-
-const paymentRoutes = require('./routes/payment');
+const dotenv = require('dotenv'); // Import dotenv first
 const cors = require('cors');
 
+
+dotenv.config(); 
+
+const paymentRoutes = require('./routes/payment');
+
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use('/api/payments', paymentRoutes);
 
+// Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Payment Service running on port ${PORT}`));
